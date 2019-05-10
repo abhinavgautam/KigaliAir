e2f2b823cd55393af37ebd622736587b1e3c7005
Kaboya <timothy.kaboya@gmail.com>
2019-05-10 23:45:06 +0200

    Springboot template project

---
+++ src/main/java/com/garage48/kigaliair/KigaliairApplication.java
@@ -0,0 +1,13 @@
+package com.garage48.kigaliair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KigaliairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KigaliairApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/garage48/kigaliair/controller/KigaliairController.java
@@ -0,0 +1,27 @@
+package com.garage48.kigaliair.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Date;
+
+@Controller
+public class KigaliairController {
+    private String appMode;
+
+    @Autowired
+    public KigaliairController(Environment environment){
+        appMode = environment.getProperty("app-mode");
+    }
+
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("datetime", new Date());
+        model.addAttribute("username", "murenzi");
+        model.addAttribute("mode", appMode);
+
+        return "index";
+    }
+}
